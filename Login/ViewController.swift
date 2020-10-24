31f244f5a248b541d85ea8f3b3d731b7d98bf7b7
Safonova Svetlana <Safonova_s@icloud.com>
2020-10-24 22:57:39 +0300

    до кнопки назад

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textName: UITextField!
+    @IBOutlet var textPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super .touchesBegan(touches, with: event)
+    }
+    
+    @IBAction func buttonLogInn() {
+        if textName.text != "Светлана" {
+            showMessage(title: "Ошибка", message: "Неверное имя пользователя")
+        }
+        if textPassword.text != "123" {
+            showMessage(title: "Ошибка", message: "Неверный пароль")
+        }
+        
+        
+    }
+    
+    @IBAction func buttonName() {
+        showMessage( title: "Ваше имя:",  message: "Светлана")
+    }
+    
+    @IBAction func buttonPassword() {
+        showMessage(title: "Ваш пароль:", message: "123")
+    }
 
 }
 
+extension ViewController{
+    private func showMessage (title: String,  message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
